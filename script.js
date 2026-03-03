0ab3a879176bc003d634e85e85d839cabe29aa58
WILLIANMC123 <matheus.camargo.willian@escola.pr.gov.br>
2026-03-03 15:00:12 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const l1 = ["Amanda", "Bruno", "Alberto", "Carolina", "Lia", "Ricardo"];
+const l2 = [];
+const l3 = [];
+let cont_a = 0;
+
+// Processamento inicial (equivalente ao primeiro loop do Python)
+l1.forEach(nome => {
+    const nomeUpper
